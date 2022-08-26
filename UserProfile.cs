e4cdd80044df39a2ae6542582ff8662584f0faa2
Yunhao Guan <52128954+acse-yg1520@users.noreply.github.com>
2022-08-26 17:22:51 +0800

    Create project

---
+++ UserProfile.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// This is our application state. Just a regular serializable .NET class.
+    /// </summary>
+    public class UserProfile
+    {
+       
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+        public string Image { get; set; }
+    }
+}
